c8aa743f7ef72e83b679c6e2f03cbe422712fb1a
nayan-builds <c2020083@hallam.shu.ac.uk>
2024-02-22 11:08:31 +0000

    Added menu to AR Scene

--- frontend/App.tsx
+++ frontend/App.tsx
@@ -3,10 +3,15 @@ import {
   ViroARScene,
   ViroARSceneNavigator,
   ViroAmbientLight,
-  ViroText,
 } from '@viro-community/react-viro';
 import React, {useState} from 'react';
-import {StyleSheet} from 'react-native';
+import {
+  Pressable,
+  StyleSheet,
+  Text,
+  View,
+  useWindowDimensions,
+} from 'react-native';
 import {BarCodeReadEvent} from 'react-native-camera';
 import QRCodeScanner from 'react-native-qrcode-scanner';
 
@@ -49,9 +54,96 @@ export default () => {
     return <QRCodeScanner reactivate={true} onRead={onRead} />;
   } else {
     return (
-      <ViroARSceneNavigator autofocus={true} initialScene={{scene: SceneAR}} />
+      <>
+        <ViroARSceneNavigator
+          autofocus={true}
+          initialScene={{scene: SceneAR}}
+        />
+        <View style={{position: 'absolute', left: 0, bottom: 0}}>
+          <MoreInfo />
+        </View>
+      </>
     );
   }
 };
 
-const styles = StyleSheet.create({});
+function MoreInfo() {
+  const windowWidth = useWindowDimensions().width;
+  const margin = 10;
+
+  return (
+    <View
+      style={[
+        styles.menuBackground,
+        {width: windowWidth - 2 * margin, margin},
+      ]}>
+      <Text style={styles.menuHeading}>Want more information about me?</Text>
+      <CustomButton
+        text="Work History"
+        onPress={() => console.log('Pressed')}
+      />
+      <CustomButton
+        text="Education History"
+        onPress={() => console.log('Pressed')}
+      />
+      <CustomButton
+        text="Hobbies and Interests"
+        onPress={() => console.log('Pressed')}
+      />
+      <CustomButton
+        text="Contact Info"
+        onPress={() => console.log('Pressed')}
+      />
+    </View>
+  );
+}
+
+function CustomButton({text, onPress}: {text: string; onPress: () => void}) {
+  return (
+    <Pressable
+      style={({pressed}) => [
+        {
+          backgroundColor: pressed ? 'rgb(100, 100, 100)' : 'rgb(30, 30, 30)',
+        },
+        styles.button,
+      ]}
+      onPress={onPress}
+      //hitSlop is used to increase the touchable area of the button
+      hitSlop={15}
+      //pressRetentionOffset is used to increase the distance that you need to move your finger before the button is unpressed
+      pressRetentionOffset={25}>
+      <Text style={styles.buttonText}>{text}</Text>
+    </Pressable>
+  );
+}
+
+const styles = StyleSheet.create({
+  button: {
+    margin: 10,
+    padding: 10,
+    elevation: 2,
+    opacity: 1,
+    borderRadius: 10,
+  },
+  buttonText: {
+    color: 'white',
+    fontWeight: 'bold',
+    fontSize: 15,
+    textAlign: 'center',
+  },
+  menuBackground: {
+    flexDirection: 'column',
+    alignItems: 'stretch',
+    backgroundColor: 'rgba(0, 0, 0, 0.3)',
+    borderRadius: 10,
+    borderWidth: 3,
+    borderColor: 'rgba(0, 0, 0, 0.5)',
+  },
+  menuHeading: {
+    color: 'white',
+    textAlign: 'center',
+    fontSize: 20,
+    fontWeight: 'bold',
+    padding: 10,
+  },
+});
